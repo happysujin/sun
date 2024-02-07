349a023365b5b12f690068d86133258b36363428
happysujin <50164158+happysujin@users.noreply.github.com>
2024-02-07 14:22:13 +0900

    planet description 내용 추가

--- information.js
+++ information.js
@@ -1,3 +1,17 @@
+var planetInfoDescription = [
+    { name: "Sun", diameter: 0, radius: 0, angle: 0, orbitalPeriod: 1},
+    { name: "Mercury", diameter: 2439.70, radius: 0.39, angle: 0, orbitalPeriod: 88 }, // 공전 주기는 일 단위로 지정
+    { name: "Venus", diameter: 6051.80, radius: 0.7, angle: 0, orbitalPeriod: 225 },
+    { name: "Earth", diameter: 6378.14, radius: 1, angle: 0, orbitalPeriod: 365 },
+    { name: "Mars", diameter: 3396.20, radius: 1.52, angle: 0, orbitalPeriod: 687 },
+    { name: "Jupiter", diameter: 71492, radius: 5, angle: 0, orbitalPeriod: 4333 },
+    { name: "Saturn", diameter: 60268, radius: 10, angle: 0, orbitalPeriod: 10759 },
+    { name: "Uranus", diameter: 25559, radius: 20, angle: 0, orbitalPeriod: 30687 },
+    { name: "Neptune", diameter: 24764, radius: 30, angle: 0, orbitalPeriod: 60190 }
+];
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var infoIcon = document.getElementById('infoIcon');
     var description = document.getElementById('description');
@@ -43,12 +57,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function planetInfoRegister() {
     // h2 요소 내용 변경
-    var one = planets[track];
+    var one = planetInfoDescription[track];
     var h2Element = document.querySelector('#description h2');
     h2Element.textContent = one.name;
 
     // p 요소 내용 변경
     var pElement = document.querySelector('#description p');
-    pElement.textContent = 'Diameter: ' + one.diameter + '\n';
+    pElement.textContent = 'Diameter: ' + one.diameter + " (km) \r\n Radius: " + one.radius + ' (AU)\r\nOrbital Period: ' + one.orbitalPeriod + ' (day)';;
 }
-
